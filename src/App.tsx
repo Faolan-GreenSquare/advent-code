008cedd26e9b73cf464eb4243886278f9ba6d075
Travis Winfield <travis.winfield@velrada.com>
2022-12-05 13:45:12 +0800

    Day 5 set up initial loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent } from "react"
 
 export const App = () => {
-  const [output, setOutput] = React.useState(0);
+  const [output, setOutput] = React.useState('');
   const [input, setInput] = React.useState('');
 
   const handleOnFileChange = (e) => {
@@ -28,24 +28,42 @@ export const App = () => {
   )
 }
 
-const executeCalculations = (input: string): number => {
+const executeCalculations = (input: string): string => {
   if(!input){
-    return 0;
+    return '';
   }
   const rows = input.split('\r\n');
-  let output = 0;
+  let output = '';
+  /*
+      [H]         [D]     [P]        
+  [W] [B]         [C] [Z] [D]        
+  [T] [J]     [T] [J] [D] [J]        
+  [H] [Z]     [H] [H] [W] [S]     [M]
+  [P] [F] [R] [P] [Z] [F] [W]     [F]
+  [J] [V] [T] [N] [F] [G] [Z] [S] [S]
+  [C] [R] [P] [S] [V] [M] [V] [D] [Z]
+  [F] [G] [H] [Z] [N] [P] [M] [N] [D]
+  1   2   3   4   5   6   7   8   9 
+  */
+  
+  const arr = [
+    ['_'], // 0
+    ['F', 'C', 'J', 'P', 'H', 'T', 'W'], // 1
+    ['G','R','V','F','Z','J','B','H'], // 2
+    ['H','P','T','R'], // 3
+    ['Z','S','N','P','H','T'], // 4
+    ['N','V','F','Z','H','J','C','D'], // 5
+    ['P','M','G','F','W','D','Z'], // 6
+    ['M','V','Z','W','S','J','D','P'], // 7
+    ['N','D','S'], // 8
+    ['D','Z','S','F','M'] // 9
+  ]
 
-  for(let i = 0; i < rows.length; i=i+3) {
-    for(let j = 0; j < rows[i].length; j++){
-      if(rows[i+1].includes(rows[i][j]) && rows[i+2].includes(rows[i][j])){
-        if (rows[i][j] === rows[i][j].toUpperCase()){
-          output = output + (rows[i][j].charCodeAt(0) - 38);
-        } else {
-          output = output + (rows[i][j].charCodeAt(0) - 96);
-        }
-        break;
-      }
-    }
+  for(let i = 10; i < rows.length; i++){
+    const instructions = rows[i].split(' ');
+    const qty = instructions[1];
+    const source = instructions[3];
+    const destination = instructions[5];
   }
 
   return output;  
