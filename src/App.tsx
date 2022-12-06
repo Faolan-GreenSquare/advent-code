f72e5bb4e22a3cf9d17e9ad23d5f13b623c20b20
Travis Winfield <travis.winfield@velrada.com>
2022-12-06 13:45:21 +0800

    part two

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ export const App = () => {
   }
 
   React.useEffect(() => {
-    setOutput(executepartOne(input))
+    //setOutput(executepartOne(input));
+    setOutput(executepartTwo(input));
   }, [input]);
 
   return (
@@ -46,6 +47,45 @@ const executepartOne = (input: string): number => {
     })
     if (isUnique) {
       output = i + 4;
+      break;
+    }
+  }
+  return output;
+}
+
+const executepartTwo = (input: string): number => {
+  if (!input) {
+    return 0;
+  }
+  //const rows = input.split('\r\n');
+  let output = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const arr: string[] = [
+      input[i + 0], 
+      input[i + 1], 
+      input[i + 2], 
+      input[i + 3],
+      input[i + 4], 
+      input[i + 5], 
+      input[i + 6], 
+      input[i + 7], 
+      input[i + 8], 
+      input[i + 9], 
+      input[i + 10],
+      input[i + 11], 
+      input[i + 12], 
+      input[i + 13]
+    ]
+    let isUnique = true;
+
+    arr.forEach((element) => {
+      if(!(arr.filter((x) => x != element).length === 13)){
+        isUnique = false;
+      }
+    })
+    if (isUnique) {
+      output = i + 14;
       break;
     }
   }
