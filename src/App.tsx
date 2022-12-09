be96a7e37de56e32f81443cd73968a294959980b
Travis Winfield <travis.winfield@velrada.com>
2022-12-09 17:52:47 +0800

    starting part one

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react"
+import React from "react"
 
 export const App = () => {
   const [output, setOutput] = React.useState(0);
@@ -30,8 +30,76 @@ export const App = () => {
 }
 
 const execute = (input: string): number => {
-  if(!input){return 0;}
+  if (!input) { return 0; }
   const rows = input.split('\r\n');
   let output = 0;
-  return output;  
+  let current_x = 0;
+  let current_y = 0;
+  let max_x = 0;
+  let min_x = 0;
+  let max_y = 0;
+  let min_y = 0;
+
+  // Build grid
+  for (let i = 0; i < rows.length; i++) {
+    const instruction = rows[i].split(' ')[0];
+    const moves = parseInt(rows[i].split(' ')[1]);
+    switch (instruction.toUpperCase()) {
+      case "U":
+        current_y = current_y + moves;
+        max_y = current_y > max_y ? current_y : max_y;
+        break;
+      case "D":
+        current_y = current_y - moves;
+        min_y = current_y < min_y ? current_y : min_y;
+        break;
+      case "R":
+        current_x = current_x + moves;
+        max_x = current_x > max_x ? current_x : max_x;
+        break;
+      case "L":
+        current_x = current_x - moves;
+        min_x = current_x < min_x ? current_x : min_x;
+        break;
+    }
+  }
+
+  const gridHeight = max_y - min_y;
+  const gridWidth = max_x - min_x;
+  const board: boolean[][] = [];
+
+  for (let i = 0; i < gridHeight; i++) {
+    board[i] = [];
+    for (let j = 0; j < gridWidth; j++) {
+      board[i][j] = false;
+    }
+  }
+
+  let head_y = gridHeight;
+  let tail_y = gridHeight;
+  let tail_x = 0;
+  let head_x = 0;  
+
+  // workout
+  for (let i = 0; i < rows.length; i++) {
+    const instruction = rows[i].split(' ')[0];
+    const moves = parseInt(rows[i].split(' ')[1]);
+    switch (instruction.toUpperCase()) {
+      case "U":
+        head_y = head_y - moves;
+        break;
+      case "D":
+        head_y = head_y + moves;
+        break;
+      case "R":
+        head_x = head_x + moves;
+        break;
+      case "L":
+        head_x = head_x - moves;
+        break;
+    }
+    
+  }
+
+  return output;
 }
