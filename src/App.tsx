0ac7bf2e588b0b2f356f07284a8cb8cf93843507
Travis Winfield <twinfield@live.com>
2022-12-10 18:34:10 +0800

    loop does not work for part two

--- src/App.tsx
+++ src/App.tsx
@@ -36,17 +36,21 @@ const execute = (input: string): number => {
 
   try {
     const positions: Record<string, boolean> = { "0,0": true };
-    let tail_x = 0;
-    let tail_y = 0;
-    let head_x = 0;
-    let head_y = 0;
-    let rope_x = 0;
-    let rope_y = 0;
+    const rope: Record<number, string> = { 0: '0,0', 1: '0,0', 2: '0,0', 3: '0,0', 4: '0,0', 5: '0,0', 6: '0,0', 7: '0,0', 8: '0,0', 9: '0,0' };
+
 
     for (let i = 0; i < rows.length; i++) {
       const instruction = rows[i].split(' ')[0];
       const moves = parseInt(rows[i].split(' ')[1]);
+      let temp_x = 0;
+      let temp_y = 0;
+
       for (let j = 1; j <= moves; j++) {
+        let head_x = parseInt(rope[0].split(',')[0]);
+        let head_y = parseInt(rope[0].split(',')[1]);
+        temp_x = head_x;
+        temp_y = head_y;
+
         switch (instruction.toUpperCase()) {
           case "U": // Y + 1
             head_y++;
@@ -61,22 +65,31 @@ const execute = (input: string): number => {
             head_x--;
             break;
         }
-        const diff_x = tail_x - head_x;
-        const diff_y = tail_y - head_y;
-        if (!(diff_x >= -1 && diff_x <= 1 && diff_y >= -1 && diff_y <= 1)) {
-          tail_x = rope_x;
-          tail_y = rope_y;
-          positions[`${tail_x},${tail_y}`] = true;
+        rope[0] = `${head_x},${head_y}`;
+        for (let k = 1; k < 10; k++) {
+          let leading_x = parseInt(rope[k - 1].split(',')[0]);
+          let leading_y = parseInt(rope[k - 1].split(',')[1]);
+          let trailing_x = parseInt(rope[k].split(',')[0]);
+          let trailing_y = parseInt(rope[k].split(',')[1]);
+
+          const diff_x = trailing_x - leading_x;
+          const diff_y = trailing_y - leading_y;
+
+          if (!(Math.abs(diff_x) <= 1 && Math.abs(diff_y) <= 1)) {
+            rope[k] = `${temp_x},${temp_y}`;
+            if (k === 9) {
+              debugger;
+              positions[`${temp_x},${temp_y}`] = true;
+            }
+            temp_x = trailing_x;
+            temp_y = trailing_y;
+          }
         }
-        rope_x = head_x;
-        rope_y = head_y;
       }
     }
-    for (let x of Object.keys(positions)){
-      if(!!x){
-        output++;
-      }
-    }
+    Object.keys(positions).forEach((x) => {
+      output++;
+    });
   } catch (e) {
     console.log(e)
     debugger;
