74c79caf7136d406eb5752af4368bf49ff72aedd
Travis Winfield <twinfield@live.com>
2022-12-10 09:27:25 +0800

    Latest submission

--- src/App.tsx
+++ src/App.tsx
@@ -39,67 +39,92 @@ const execute = (input: string): number => {
   let min_x = 0;
   let max_y = 0;
   let min_y = 0;
-
-  // Build grid
-  for (let i = 0; i < rows.length; i++) {
-    const instruction = rows[i].split(' ')[0];
-    const moves = parseInt(rows[i].split(' ')[1]);
-    switch (instruction.toUpperCase()) {
-      case "U":
-        current_y = current_y + moves;
-        max_y = current_y > max_y ? current_y : max_y;
-        break;
-      case "D":
-        current_y = current_y - moves;
-        min_y = current_y < min_y ? current_y : min_y;
-        break;
-      case "R":
-        current_x = current_x + moves;
-        max_x = current_x > max_x ? current_x : max_x;
-        break;
-      case "L":
-        current_x = current_x - moves;
-        min_x = current_x < min_x ? current_x : min_x;
-        break;
+  try {
+    // Build grid
+    for (let i = 0; i < rows.length; i++) {
+      const instruction = rows[i].split(' ')[0];
+      const moves = parseInt(rows[i].split(' ')[1]);
+      switch (instruction.toUpperCase()) {
+        case "U":
+          current_y = current_y + moves;
+          max_y = current_y > max_y ? current_y : max_y;
+          break;
+        case "D":
+          current_y = current_y - moves;
+          min_y = current_y < min_y ? current_y : min_y;
+          break;
+        case "R":
+          current_x = current_x + moves;
+          max_x = current_x > max_x ? current_x : max_x;
+          break;
+        case "L":
+          current_x = current_x - moves;
+          min_x = current_x < min_x ? current_x : min_x;
+          break;
+      }
     }
-  }
 
-  const gridHeight = max_y - min_y;
-  const gridWidth = max_x - min_x;
-  const board: boolean[][] = [];
+    const gridHeight = max_y + min_y;
+    const gridWidth = max_x + min_x;
+    const board: boolean[][] = [];
 
-  for (let i = 0; i < gridHeight; i++) {
-    board[i] = [];
-    for (let j = 0; j < gridWidth; j++) {
-      board[i][j] = false;
+    for (let i = 0; i < gridWidth; i++) {
+      board[i] = [];
+      for (let j = 0; j < gridHeight; j++) {
+        board[i][j] = false;
+      }
     }
-  }
 
-  let head_y = gridHeight;
-  let tail_y = gridHeight;
-  let tail_x = 0;
-  let head_x = 0;  
+    let head_y = gridHeight-1;
+    let tail_y = gridHeight-1;
+    let tail_x = 0;
+    let head_x = 0;
+    board[tail_x][tail_y] = true;
+    // workout
+    for (let i = 0; i < rows.length; i++) {
+      const instruction = rows[i].split(' ')[0];
+      const moves = parseInt(rows[i].split(' ')[1]);
+      for (let j = 0; j < moves; j++) {
+        const old_x = head_x
+        const old_y = head_y
+        switch (instruction.toUpperCase()) {
+          case "U":
+            head_y = head_y - 1;
+            break;
+          case "D":
+            head_y = head_y + 1;
+            break;
+          case "R":
+            head_x = head_x + 1;
+            break;
+          case "L":
+            head_x = head_x - 1;
+            break;
+        }
+        // is H far enough away from T to warrant T moving
+        // if Yes, T = H old position
+        const diff_x = head_x - tail_x;
+        const diff_y = head_y - tail_y;
+        if (!(diff_x >= -1 && diff_x <= 1 && diff_y >= -1 && diff_y <= 1)) {
+          tail_x = old_x;
+          tail_y = old_y;
+          board[tail_x][tail_y] = true;
+        }
+      }
 
-  // workout
-  for (let i = 0; i < rows.length; i++) {
-    const instruction = rows[i].split(' ')[0];
-    const moves = parseInt(rows[i].split(' ')[1]);
-    switch (instruction.toUpperCase()) {
-      case "U":
-        head_y = head_y - moves;
-        break;
-      case "D":
-        head_y = head_y + moves;
-        break;
-      case "R":
-        head_x = head_x + moves;
-        break;
-      case "L":
-        head_x = head_x - moves;
-        break;
+      for (let i = 0; i < gridWidth; i++) {
+        for (let j = 0; j < gridHeight; j++) {
+          if (!!board[i][j]) {
+            output++;
+          }
+        }
+      }
     }
-    
+  } catch (e) {
+    console.log(e);
+    debugger;
   }
+  
 
   return output;
 }
