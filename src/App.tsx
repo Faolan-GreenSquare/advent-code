e7938b65ea87aa4da5dbc40e11b72bf7dedad419
Travis Winfield <twinfield@live.com>
2022-12-13 20:01:41 +0800

    Initial attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,90 @@
 import React from "react"
 
 export const App = () => {
+  const [output, setOutput] = React.useState(0);
+  const [input, setInput] = React.useState('');
 
-  const output = execute();
+  const handleOnFileChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+      if (typeof (reader.result) == 'string') {
+        setInput(reader.result);
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    setOutput(execute(input));
+  }, [input]);
 
   return (
     <div>
+      <div>Select File:</div>
+      <div><input type='file' onChange={handleOnFileChange}></input></div>
       <div>Output: {output}</div>
     </div>
   )
 }
 
-const execute = (): number => {
-
+const execute = (input: string): number => {
+  if (!input) { return 0; }
+  const rows = input.split('\r\n');
   let output = 0;
 
-  interface Monkey {
-    items: number[],
-    itemsInspected: number,
-    operation: {
-      x: string | number,
-      y: string | number,
-      op: string
-    },
-    test: number,
-    targets: number[]
+  interface mountain {
+    height: number;
+    adj: number[];
   }
 
-  const monkeys: Record<number, Monkey> = {
-    0: { items: [79, 98], itemsInspected: 0, operation: { x: "old", y: 19, op: "*" }, test: 23, targets: [3, 2] },
-    1: { items: [54, 65, 75, 74], itemsInspected: 0, operation: { x: "old", y: 6, op: "+" }, test: 19, targets: [0, 2] },
-    2: { items: [79, 60, 97], itemsInspected: 0, operation: { x: "old", y: "old", op: "*" }, test: 13, targets: [3, 1] },
-    3: { items: [74], itemsInspected: 0, operation: { x: "old", y: 3, op: "+" }, test: 17, targets: [1, 0] }
-  }
+  let startV = 0;
+  let endV = 0;
+  const range: Record<number, mountain> = {};
+  let heightConversion = "abcdefghijklmnopqrstuvwxyz";
 
-  const bigMod = 23*19*13*17;
-
-  /*
-  const monkeys: Record<number, Monkey> = {
-    0: { items: [65, 78], itemsInspected: 0, operation: { x: "old", y: 3, op: "*" }, test: 5, targets: [3, 2] },
-    1: { items: [54, 78, 86, 79, 73, 64, 85, 88], itemsInspected: 0, operation: { x: "old", y: 8, op: "+" }, test: 11, targets: [7, 4] },
-    2: { items: [69, 97, 77, 88, 87], itemsInspected: 0, operation: { x: "old", y: 2, op: "+" }, test: 2, targets: [3, 5] },
-    3: { items: [99], itemsInspected: 0, operation: { x: "old", y: 4, op: "+" }, test: 13, targets: [5, 1] },
-    4: { items: [60, 57, 52], itemsInspected: 0, operation: { x: "old", y: 19, op: "*" }, test: 7, targets: [6, 7] },
-    5: { items: [91, 82, 85, 73, 84, 53], itemsInspected: 0, operation: { x: "old", y: 5, op: "+" }, test: 3, targets: [1, 4] },
-    6: { items: [88, 74, 68, 56], itemsInspected: 0, operation: { x: "old", y: "old", op: "*" }, test: 17, targets: [2, 0] },
-    7: { items: [54, 82, 72, 71, 53, 99, 67], itemsInspected: 0, operation: { x: "old", y: 1, op: "+" }, test: 19, targets: [0, 6] }
-  }
-  */
-
-  const rounds = 10000;
-  for (let i = 0; i < rounds; i++) {
-    for (const key in monkeys) {
-      const monkey = monkeys[key];
-      for (let item = 0; item < monkey.items.length; item++) {
-        monkey.itemsInspected++;
-        const x: number = typeof (monkey.operation.x) === 'string' ? monkey.items[item] : monkey.operation.x;
-        const y: number = typeof (monkey.operation.y) === 'string' ? monkey.items[item] : monkey.operation.y;
-        const doMaths = {
-          '+': function (x, y): number { return x + y },
-          '-': function (x, y): number { return x - y },
-          '*': function (x, y): number { return x * y },
-          '/': function (x, y): number { return x / y }
-        }
-        monkey.items[item] = Math.floor(doMaths[monkey.operation.op](x, y) / bigMod);
-        monkeys[monkey.targets[monkey.items[item] % monkey.test === 0 ? 1 : 0]].items.push(monkey.items[item]);
+  // Build Mountain Range
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < rows[i].length; j++) {
+      if (rows[i][j] === 'S') {
+        startV = i * rows[i].length + j % rows[i].length;
       }
-      monkey.items = [];
-    }
-    if(i === 0 || i === 19 || i === 999 || i === 1999 || i === 2999 || i === 3999 || i === 4999 || i === 5999 || 
-      i === 6999 || i === 7999 || i === 8999 || i === 9999) {
-      console.log(`Round: ${i+1}`)
-      console.table(monkeys);
+      else if (rows[i][j] === 'E') {
+        endV = i * rows[i].length + j % rows[i].length;
+      }
+      range[i * rows[i].length + j % rows[i].length] = {
+        height: rows[i][j] === 'S'
+          ? 0
+          : rows[i][j] === 'E'
+            ? 25
+            : heightConversion.indexOf(rows[i][j]),
+        adj: []
+      };
     }
   }
+
+  // Add Adjacencies
+  for (let i = 0; i < Object.keys(range).length; i++) {
+    const x = i % rows[0].length;
+    const y = Math.floor(i / rows[0].length);
+
+    // Check North y--
+    if (y !== 0 && range[(y - 1) * rows[0].length + x].height <= range[i].height + 1)
+      range[i].adj.push((y - 1) * rows[0].length + x);
+
+    // Check South y++
+    if (y !== rows.length - 1 && range[(y + 1) * rows[0].length + x].height <= range[i].height + 1)
+      range[i].adj.push((y + 1) * rows[0].length + x);
+
+    // Check West x--
+    if (x !== 0 && range[y * rows[0].length + x - 1].height <= range[i].height + 1)
+      range[i].adj.push(y * rows[0].length + x - 1);
+
+    // Check East x++
+    if (x !== rows[0].length - 1 && range[y * rows[0].length + x + 1].height <= range[i].height + 1)
+      range[i].adj.push(y * rows[0].length + x + 1);
+  }
+
+  
   return output;
 }
